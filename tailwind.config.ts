bd08e505a4f3db61471e823b23508dd6137f7cd0
manoj1983 <30681241+manoj1983@users.noreply.github.com>
2025-11-12 15:46:13 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,15 @@
-
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
+
+export default config;
 
 export default {
 	darkMode: ["class"],
